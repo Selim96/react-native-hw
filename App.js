46523a74b0523be1d74709404a20f77a7ba3593f
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-24 01:56:12 +0300

    add input

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const inputChange = (text) => setValue(text);
+
   return (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder='Type text'
+          value={value}
+          keyboardType='default'
+          onChangeText={inputChange}
+          />
+      </KeyboardAvoidingView>
       <StatusBar style="auto" />
     </View>
+  </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#009A50',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#FF6C00',
+  },
+  input: {
+    color: 'fff',
+    border: 'solid',
+    borderColor: '#FF6D99',
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 10,
+    marginTop: 50
+  }
 });
